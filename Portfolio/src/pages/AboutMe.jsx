3df3f6c78225b493e4efe7d0b2c3ec3b3ed8877d
Spencer Cox <spencox@gmail.com>
2024-02-20 19:30:16 -0600

    added about me section, worked on form. Got form elements in place. Working on error messages

--- Portfolio/src/pages/AboutMe.jsx
+++ Portfolio/src/pages/AboutMe.jsx
@@ -1,15 +1,16 @@
 export default function AboutMe() {
     return (
       <div>
-        <h1>About Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
+        <h1 className="p-3">About Page</h1>
+            <p className="p-3">
+                Hello, I'm Spencer Cox, a passionate and driven web developer on a mission to turn my love for coding into a rewarding career. With a background in computer science and ongoing education through coding boot camps and certification programs at the University of Texas at Austin, I'm well-equipped with the technical knowledge and skills needed to excel in web development. At 37 years old, I've embarked on an exciting career shift, dedicating myself to the ever-evolving world of web development.
+            </p>
+            <p className="p-3">
+                I find immense joy in crafting immersive web experiences, from pixel-perfect front-end designs to robust back-end functionalities. What sets me apart is my unique blend of theoretical computer science knowledge and hands-on development expertise. I've honed my skills through real-world projects and have built a diverse portfolio that showcases my ability to create dynamic and responsive websites. Whether it's enhancing user experiences or solving complex technical challenges, I'm committed to pushing the boundaries of what's possible in web development.
+            </p>
+            <p className="p-3">
+                As I continue to learn and grow in this dynamic field, I'm eager to collaborate with like-minded professionals, contribute to innovative projects, and make a meaningful impact on the digital landscape. Join me on this exciting journey as we explore the limitless possibilities of web development together.
+            </p>
       </div>
     );
   }
